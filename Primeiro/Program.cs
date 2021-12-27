ebd5a6821044aa3ddfddec74560d8c441fe3da33
vitorcoke <cokevitor@gmail.com>
2021-12-27 20:34:13 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Primeiro
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var a = "hellow";
+            var b = "Word";
+            Console.WriteLine(a + b);
+        }
+    }
+}
